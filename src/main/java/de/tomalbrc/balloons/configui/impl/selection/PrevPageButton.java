47284a977491a23190dd95b8dfe36ff8ca19b7fa
tomalbrc <tomalbrc@me.com>
2025-12-02 23:34:40 +0100

    fix: incorrect gui element class

--- src/main/java/de/tomalbrc/balloons/configui/impl/selection/PrevPageButton.java
+++ src/main/java/de/tomalbrc/balloons/configui/impl/selection/PrevPageButton.java
@@ -1,15 +1,15 @@
 package de.tomalbrc.balloons.configui.impl.selection;
 
-import com.cobblemonislands.emotive.config.ConfiguredAnimation;
+import de.tomalbrc.balloons.config.ConfiguredBalloon;
 import de.tomalbrc.balloons.configui.api.ConfiguredGui;
 import de.tomalbrc.balloons.configui.api.GuiElementData;
 import de.tomalbrc.balloons.configui.api.GuiElementType;
 import eu.pb4.sgui.api.elements.GuiElementBuilder;
 import net.minecraft.world.item.Items;
 
-public class PrevPageButton implements GuiElementType<GuiElementData, ConfiguredAnimation> {
+public class PrevPageButton implements GuiElementType<GuiElementData, ConfiguredBalloon> {
     @Override
-    public GuiElementBuilder build(ConfiguredGui<GuiElementData, ConfiguredAnimation> g, GuiElementData data) {
+    public GuiElementBuilder build(ConfiguredGui<GuiElementData, ConfiguredBalloon> g, GuiElementData data) {
         if (g.getCurrentPage("contents") == 0) {
             return new GuiElementBuilder(Items.AIR.getDefaultInstance());
         }
